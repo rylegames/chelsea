13af2b2e070003175376ed55cc1d4b86baba41b6
Ryan Lee <rylee.nyc@gmail.com>
2020-06-01 09:32:18 -0400

    projects

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Layout from "../../components/Layout";
-import BlogRoll from "../../components/BlogRoll";
+import ProjectRoll from "../../components/ProjectRoll";
 
 export default class ProjectIndexPage extends React.Component {
   render() {
@@ -28,7 +28,7 @@ export default class ProjectIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <BlogRoll />
+              <ProjectRoll />
             </div>
           </div>
         </section>
